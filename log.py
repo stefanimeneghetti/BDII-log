230b66a2572a4e937223c7c7c7f89f0578a83bde
Stefani Meneghetti <meneghettistefani@gmail.com>
2022-02-07 22:51:08 -0300

    Corrige bugs

--- log.py
+++ log.py
@@ -8,6 +8,7 @@ class Log:
     findCheckpoint = False
     transactionChanges = {}
     outstandingTransactions = []
+    findStart = []
     findTransactionStart = 0
 
     def __init__(self, log: list, tableColumns: list, db: DB) -> None:
@@ -18,24 +19,31 @@ class Log:
     def parse(self) -> None:
         for line in self.log:
             line = line[1:].replace('>', '').lower()
-
+            
             if line.startswith('commit'):
                 self.commitedTransactions.append(line.split()[1])
             elif line.startswith('start') and 'ckpt' not in line and not self.findCheckpoint:
                 self.transactions.append(line.split()[1])
-                if line.split()[1] in self.outstandingTransactions:
+                if line.split()[1] in self.findStart:
                     self.findTransactionStart += 1
             elif line.startswith('start') and self.lastCheckpoint:
                 if (self.validCheckpoint):
                     self.findCheckpoint = True
                     self.outstandingTransactions = [x.strip() for x in line[line.index('(') + 1:line.index(')')].split(',')]
                     self.transactions += self.outstandingTransactions
+
+                    for i in range (len(self.outstandingTransactions)):
+                        if self.outstandingTransactions[i] in self.commitedTransactions:
+                            self.findStart.append(self.outstandingTransactions[i])
+                elif 'ckpt' not in line:
+                    if line.split()[1] in self.findStart:
+                        self.findTransactionStart += 1
                 self.validCheckpoint = False
             elif line == 'end ckpt':
                 if self.lastCheckpoint == False:
                     self.lastCheckpoint = True  
                     self.validCheckpoint = True
-            else:
+            elif not (line.startswith('start') and not self.endCheckpoint) and 'crash' not in line:
                 operations = line.split(',')
                 if self.transactionChanges.get(operations[0]) == None:
                     self.transactionChanges[operations[0]] = {}
@@ -44,7 +52,7 @@ class Log:
                 if self.transactionChanges[operations[0]][operations[1]].get(operations[2]) == None:
                     self.transactionChanges[operations[0]][operations[1]][operations[2]] = operations[3]
                            
-            if (self.findCheckpoint) and self.findTransactionStart == len(self.outstandingTransactions): # terminou de ler a parte necessaria do log
+            if (self.findCheckpoint) and self.findTransactionStart == len(self.findStart): # terminou de ler a parte necessaria do log
                 break
 
     def showResults(self) -> None:
